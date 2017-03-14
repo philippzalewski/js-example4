7ddfdc842585794c513df811788597ad094590e8
Philipp Zalewski <kontakt@centrum-ngo.pl>
2017-03-14 18:47:06 +0100

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// plik scripts.js
+
+var a = prompt('Wpisz wartość podstawy trójkąta - "a" ');
+var h = prompt('Wpisz wartość wysokości trójkąta - "h" '); 
+var triangleArea = a*h/2;
+alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
